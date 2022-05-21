a34ae795b51f771ff900f3df4b986315a51e930f
Uriel Enrique <urielenrique99@gmail.com>
2022-05-20 20:11:47 -0500

    initial configuration done

---
+++ EmojiLibrary/DataSiurce.swift
@@ -0,0 +1,12 @@
+//
+//  DataSiurce.swift
+//  EmojiLibrary
+//
+//  Created by Uriel Hernandez Gonzalez on 20/05/22.
+//
+
+import UIKit
+
+class DataSource: NSObject {
+    let data = Emoji.shared
+}
