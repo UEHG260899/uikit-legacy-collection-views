5bf19ac15765c5076b78752d0c0434db2ec6ea1e
Uriel Enrique <urielenrique99@gmail.com>
2022-05-20 19:38:05 -0500

    Initial Commit

---
+++ EmojiLibrary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiLibrary
+//
+//  Created by Uriel Hernandez Gonzalez on 20/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
